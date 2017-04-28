301c25c3b240aa9bb50823028473a3a6030b9ff7
Geert van Horrik <geert@catenalogic.com>
2017-04-28 20:52:03 +0200

    Initial solution setup

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Orc.Prism
-Prism extensions built on top of Catel.
+
+[![Join the chat at https://gitter.im/WildGums/Orc.Prism](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/WildGums/Orc.Prism?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+
+![License](https://img.shields.io/github/license/WildGums/Orc.Prism.svg)
+![Version](https://img.shields.io/nuget/v/[NUGET.PACKAGENAME].svg)
+![Pre-release version](https://img.shields.io/nuget/vpre/Orc.Prism.svg)
+
+To be described.
---
+++ src/Orc.Prism6.Example/Orc.Prism6.Example.Shared/Views/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="WildGums">
+//   Copyright (c) 2008 - 2017 WildGums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Orc.Prism6.Views
+{
+    public partial class MainWindow 
+    {
+        #region Constructors
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        #endregion
+    }
+}
